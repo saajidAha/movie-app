72497171876e679c2abff103ae6ff5e61924bb5f
Saajid Ahamed <saajidahamed1@gmail.com>
2025-04-13 18:06:55 +0530

    remove unnecessary files + add tailwind setup

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View className='w-full h-full justify-center items-center'>
+      <Text className='text-5xl text-blue-500'>Welcome!</Text>
+    </View>
+  );
+}
