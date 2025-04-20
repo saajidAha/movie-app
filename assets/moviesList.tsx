b93851cea6de21588a9f100b79024dc238bdedd3
Saajid Ahamed <saajidahamed1@gmail.com>
2025-04-21 01:41:48 +0530

    add a list of test movie data for rendering

---
+++ assets/moviesList.tsx
@@ -0,0 +1,106 @@
+import mufasa from "@/assets/images/mufasa.png";
+
+const moviesList = [
+  {
+    title: "The Lion King",
+    image: mufasa,
+    genre: "Animation"
+  },
+  {
+    title: "Frozen",
+    image: mufasa,
+    genre: "Animation"
+  },
+  {
+    title: "Avengers: Endgame",
+    image: mufasa,
+    genre: "Action"
+  },
+  {
+    title: "Inception",
+    image: mufasa,
+    genre: "Sci-Fi"
+  },
+  {
+    title: "Titanic",
+    image: mufasa,
+    genre: "Romance"
+  },
+  {
+    title: "The Dark Knight",
+    image: mufasa,
+    genre: "Action"
+  },
+  {
+    title: "Forrest Gump",
+    image: mufasa,
+    genre: "Drama"
+  },
+  {
+    title: "The Matrix",
+    image: mufasa,
+    genre: "Sci-Fi"
+  },
+  {
+    title: "Interstellar",
+    image: mufasa,
+    genre: "Sci-Fi"
+  },
+  {
+    title: "The Godfather",
+    image: mufasa,
+    genre: "Crime"
+  },
+  {
+    title: "Pulp Fiction",
+    image: mufasa,
+    genre: "Crime"
+  },
+  {
+    title: "The Shawshank Redemption",
+    image: mufasa,
+    genre: "Drama"
+  },
+  {
+    title: "Gladiator",
+    image: mufasa,
+    genre: "Action"
+  },
+  {
+    title: "Jurassic Park",
+    image: mufasa,
+    genre: "Adventure"
+  },
+  {
+    title: "Toy Story",
+    image: mufasa,
+    genre: "Animation"
+  },
+  {
+    title: "Finding Nemo",
+    image: mufasa,
+    genre: "Animation"
+  },
+  {
+    title: "The Avengers",
+    image: mufasa,
+    genre: "Action"
+  },
+  {
+    title: "Black Panther",
+    image: mufasa,
+    genre: "Action"
+  },
+  {
+    title: "Coco",
+    image: mufasa,
+    genre: "Animation"
+  },
+  {
+    title: "Spider-Man: No Way Home",
+    image: mufasa,
+    genre: "Action"
+  }
+];
+
+export default moviesList;
